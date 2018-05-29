1bbbe4b114c28a11730913155ec697d7f45199f6
quark6789 <quark6789@gmail.com>
2018-05-29 01:32:57 -0700

    Added ability to select the wordlist to be used

--- diceware.py
+++ diceware.py
@@ -1,40 +1,50 @@
 import random
 
 
-def auto_gen():
+def auto_gen(num_digits):
     rand = random.SystemRandom()
-    num_str = ""
-    for i in range(5):
-        num_str += str(rand.randint(1, 6))
-    return int(num_str)
+    number_str = ""
+    for i in range(num_digits):
+        number_str += str(rand.randint(1, 6))
+    return int(number_str)
 
-
-wordlist = {}
+    
+# (Name, Filename, Number of dice required, Bits of entropy per word)
+wordlists = [("Large list",   "eff_large_wordlist.txt",     5, 12.9),
+             ("Short list 1", "eff_short_wordlist_1.txt",   4, 10.3),
+             ("Short list 2", "eff_short_wordlist_2_0.txt", 4, 10.3)]
+loaded_words = {}
 password = ""
 num_words = 0
 
-with open("eff_large_wordlist.txt") as file:
+print("Which list would you like to use?")
+for i in range(len(wordlists)):
+    print(str(i) + ":", wordlists[i][0])
+selected_list = int(input())
+
+with open(wordlists[selected_list][1]) as file:
     for line in file:
         (num, word) = line.split()
-        wordlist[int(num)] = word
-
+        loaded_words[int(num)] = word
+       
 while True:
     try:
-        input_text = input("What are the 5 dice numbers? "
-                "Type 'auto' to auto-generate or 'exit' to exit. ")
-        if input_text.lower() == "exit":
+        user_input = input("What are the " + str(wordlists[selected_list][2]) +
+               " dice numbers? Type 'auto' to auto-generate or 'exit' to exit. ")
+        if user_input.lower() == "exit":
             break
-        if (input_text.lower() == "auto"):
-            word_num = auto_gen()
+        if (user_input.lower() == "auto"):
+            word_num = auto_gen(wordlists[selected_list][2])
         else:
-            word_num = int(input_text)
+            word_num = int(user_input)
 
-        password += wordlist[word_num].capitalize()
+        password += loaded_words[word_num].capitalize()
         num_words += 1
 
         print()
         print(password)
-        print("Length: ", num_words, "\tEntropy: ", (num_words * 12.9), " bits")
+        print("Length: ", num_words, "\tEntropy: ",
+                (num_words * wordlists[selected_list][3]), " bits")
 
     except (KeyError, ValueError):
         print("That was not a valid number. Please try again.")
